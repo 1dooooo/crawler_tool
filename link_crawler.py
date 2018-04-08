f559e6f72618a60f3e2f71d201d88130b6929a4b
d o <d o>
2018-04-08 15:03:05 +0800

    ido add weibo

---
+++ link_crawler.py
@@ -0,0 +1,163 @@
+# -*- coding: UTF-8 -*-
+import re
+import urlparse
+import urllib2
+import time
+from datetime import datetime
+import robotparser
+import Queue
+import lxml.html
+import csv
+
+
+proxy_seter ='http://113.121.161.79:808'
+
+
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv', 'w'))
+        self.fields = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
+        self.writer.writerow(self.fields)
+
+    def __call__(self, url, html):
+        if re.search('/view/', url):
+            tree = lxml.html.fromstring(html)
+            row = []
+            for field in self.fields:
+                row.append(tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content())
+            self.writer.writerow(row)
+
+def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, headers=None, user_agent='wswp', proxy=None, num_retries=1, scrape_callback=None):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    # the queue of URL's that still need to be crawled
+    crawl_queue = [seed_url]
+    # the URL's that have been seen and at what depth
+    seen = {seed_url: 0}
+    # track how many URL's have been downloaded
+    num_urls = 0
+    rp = get_robots(seed_url)
+    throttle = Throttle(delay)
+    headers = headers or {}
+    if user_agent:
+        headers['User-agent'] = user_agent
+
+    while crawl_queue:
+        url = crawl_queue.pop()
+        depth = seen[url]
+        # check url passes robots.txt restrictions
+        if rp.can_fetch(user_agent, url):
+            throttle.wait(url)
+            html = download(url, headers, proxy=proxy, num_retries=num_retries)
+            links = []
+            if scrape_callback:
+                links.extend(scrape_callback(url, html) or [])
+
+            if depth != max_depth:
+                # can still crawl further
+                if link_regex:
+                    # filter for links matching our regular expression
+                    links.extend(link for link in get_links(html) if re.match(link_regex, link))
+
+                for link in links:
+                    link = normalize(seed_url, link)
+                    # check whether already crawled this link
+                    if link not in seen:
+                        seen[link] = depth + 1
+                        # check link is within same domain
+                        if same_domain(seed_url, link):
+                            # success! add this new link to queue
+                            crawl_queue.append(link)
+
+            # check whether have reached downloaded maximum
+            num_urls += 1
+            if num_urls == max_urls:
+                break
+        else:
+            print 'Blocked by robots.txt:', url
+
+
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+    def wait(self, url):
+        """Delay if have accessed this domain recently
+        """
+        domain = urlparse.urlsplit(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                print 'delay secends.......'
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
+
+
+def download(url, headers, proxy, num_retries, data=None):
+    print 'Downloading:', url
+    request = urllib2.Request(url, data, headers)
+    request_ip = urllib2.Request('http://ip.chinaz.com/getip.aspx',headers=headers)
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
+        #urllib2.install_opener(opener)   #安装以后，可以使用默认的 urllib2.urlopen()打开相关网页
+    try:
+        #opener.open   使用自定义的opener打开网页，区别于 install_opener 此方法将自定义的opener安装到默认
+        response = opener.open(request)
+        html = response.read()
+        code = response.code
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = ''
+        if hasattr(e, 'code'):
+            code = e.code
+            if num_retries > 0 and 500 <= code < 600:
+                # retry 5XX HTTP errors
+                html = download(url, headers, proxy, num_retries-1, data)
+        else:
+            code = None
+    return html
+
+
+def normalize(seed_url, link):
+    """Normalize this URL by removing hash and adding domain
+    """
+    link, _ = urlparse.urldefrag(link) # remove hash to avoid duplicates
+    return urlparse.urljoin(seed_url, link)
+
+
+def same_domain(url1, url2):
+    """Return True if both URL's belong to same domain
+    """
+    return urlparse.urlparse(url1).netloc == urlparse.urlparse(url2).netloc
+
+
+def get_robots(url):
+    """Initialize robots parser for this domain
+    """
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp
+        
+
+def get_links(html):
+    """Return a list of links from html 
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
+
+
+if __name__ == '__main__':
+    agent_for='Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'
+    link_crawler('http://example.webscraping.com', '/places/default/(index|view)', delay=1, num_retries=1, max_depth=-1, user_agent='agent_for',proxy=proxy_seter,scrape_callback=ScrapeCallback())
